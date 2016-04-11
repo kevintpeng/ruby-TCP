04287ead19e79b564f0d5714d20e9f41401c64c9
Kevin Peng <kpeng97@gmail.com>
2016-04-10 22:35:01 -0400

    Update server.rb

--- server.rb
+++ server.rb
@@ -44,34 +44,6 @@ class Server
       end
     end
   end
-  # def run
-  #   loop
-  #     Thread.start(@server.accept) do |client|
-  #       nickname = client.gets.chomp.to_sym
-  #       @connections[:client].each do |othername, otherclient|
-  #         if nickname == othername || client == otherclient
-  #           client.puts "Username exists"
-  #           Thread.kill self
-  #         end
-  #       end
-  #       puts "#{nickname} #{client}"
-  #       @connections[:client][:nickname] = client
-  #       client.puts "Connection established."
-  #       listen_user_message(username, client)
-  #     end
-  #   }
-  # end
-  #
-  # def listen_user_message(username, client)
-  #   loop{
-  #     msg = client.gets.chomp
-  #     @connections[:client].each do |othername, otherclient|
-  #       unless username == othername
-  #         otherclient.puts "#{username}: #{msg}"
-  #       end
-  #     end
-  #   }
-  # end
 end
 
 server = Server.new(3000, "localhost")
