d9f06b0988fba59df31bc299267077cb34254ad8
Kevin Peng <kpeng97@gmail.com>
2016-04-10 22:34:37 -0400

    Update client.rb

--- client.rb
+++ client.rb
@@ -27,25 +27,6 @@ class Client
       end
     end
   end
-  #
-  # def send
-  #   @request = Thread.new do
-  #     loop{
-  #       # IO object from console input
-  #       msg = $stdin.gets.chomp
-  #       @server.puts( msg )
-  #     }
-  #   end
-  # end
-  #
-  # def listen
-  #   @response = Thread.new do
-  #     loop{
-  #       msg = @server.gets.chomp
-  #       puts msg
-  #     }
-  #   end
-  # end
 end
 
 server = TCPSocket.open("localhost", 3000)
